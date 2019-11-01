46b60eff8189ec98d8dc87320f55dc656c95b6bc
NinoGlonti <54588901+NinoGlonti@users.noreply.github.com>
2019-11-01 02:54:00 +0100

    done

--- main.js
+++ main.js
@@ -1,18 +1,12 @@
 let game = new Game();
 var mode;
 
-
-
 function preload() {
   game.preload();
-  music = loadSound("sound/mus_low_quality.mp3")
-  rainsound = loadSound("sound/sfx_rain_low.mp3")
-  jumpsound = loadSound("sound/sfx_biker_jump.mp3")
-  collisionsound = loadSound("sound/sfx_collision.mp3")
-
-  text1 = loadImage("img/startscreen_text_biker_01.png")
-  text2 = loadImage("img/endscreen_text.png")
-
+  music = loadSound("sound/mus_low_quality.mp3");
+  rainsound = loadSound("sound/sfx_rain_low.mp3");
+  jumpsound = loadSound("sound/sfx_biker_jump.mp3");
+  collisionsound = loadSound("sound/sfx_collision.mp3");
 }
 
 function setup() {
@@ -21,56 +15,42 @@ function setup() {
   newColor = color(random(0), random(255), random(75));
   amt = 0;
 
-
   mode = 0; //GAME HAS NOT STARTED
   background(startColor);
-  game.setup()
+  game.setup();
   textSize(20);
 
-
   //START SCREEN
-
-  music.loop()
-
+  text = loadImage("img/startscreen_text_biker_01.png");
+  music.loop();
 }
-
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
-  game.resize()
+  game.resize();
 }
-
-
-
 
 function draw() {
   clear();
   if (mode == 0) {
     createCanvas(windowWidth, windowHeight);
     background("#518561");
-    image(text1, windowWidth / 3.5, windowHeight / 7, 800, 800)
+    image(text, windowWidth / 3.5, windowHeight / 7, 800, 800);
   }
 
-
-
-
   if (mode == 1) {
-    game.draw()
-
+    game.draw();
   }
 
   if (mode == 3) {
-
-
-    
-    background("#518561");
-    image(text2, windowWidth / 3.5, windowHeight / 7, 800, 800)
-    //collisionsound.play()
-
+    push();
+    fill("white");
+    textSize(60);
+    text("Game Over", windowWidth / 2.2, windowHeight / 2.5);
+    text("Press [SPACE] to play again", windowWidth / 1.8, windowHeight / 3);
+    pop();
   }
 }
-
-
 
 //START,JUMP,PLAYAGAIN
 
@@ -81,15 +61,15 @@ function keyPressed() {
 
   if (mode == 1 && keyCode === 32) {
     game.player.jump();
-    jumpsound.play()
+    jumpsound.play();
   }
 
   if (mode == 3 && keyCode === 32) {
-    mode = 1;
-    loop()
+    mode = 0;
+    loop();
   }
 
   if (Rain) {
-    rainsound.play()
+    rainsound.play();
   }
 }
